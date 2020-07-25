de3ed4687afa1f098350c7b50871686c50de0367
Daniel Colson <danieljamescolson@gmail.com>
2020-07-25 08:41:09 -0400

    Move failing pattern test to ast_test

--- actionpack/test/journey/path/pattern_test.rb
+++ actionpack/test/journey/path/pattern_test.rb
@@ -130,22 +130,6 @@ module ActionDispatch
           assert_no_match(path, "/page/loving")
         end
 
-        def test_ast_sets_regular_expressions
-          requirements = { name: /(tender|love)/, value: /./ }
-          path = Pattern.build(
-            "/page/:name/:value",
-            requirements,
-            SEPARATORS,
-            true
-          )
-
-          nodes = path.ast.root.grep(Nodes::Symbol)
-          assert_equal 2, nodes.length
-          nodes.each do |node|
-            assert_equal requirements[node.to_sym], node.regexp
-          end
-        end
-
         def test_match_data_with_group
           path = Pattern.build(
             "/page/:name",
